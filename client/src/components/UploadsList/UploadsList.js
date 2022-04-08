5af9f31cf793167198350d0eeb3a43fc9c7b321a
Jordane DEVÉMY <Jordane.devemy@gmail.com>
2022-04-08 16:37:18 +0200

    UploadsList and UploadsVideosList with conditions

--- client/src/components/UploadsList/UploadsList.js
+++ client/src/components/UploadsList/UploadsList.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import './uploadsList.scss';
 import { BACKEND_URI } from "../../config/constants";
 
 const UploadsList = ({ medias }) => {
@@ -9,14 +8,16 @@ const UploadsList = ({ medias }) => {
     <div className="row">
       <div className="col-md-12">
         <ul>
-          {medias &&
-            medias.map((video) => {
+          {medias.length > 0
+            ? medias.map((video) => {
               return (
                   <li key={video.slice(6, 19)}>
                   <a href={`${BACKEND_URI}/uploads/compressed-videos/${video}`} >{video}</a>
                   </li>
               )
-            })}
+            })
+            : <p>Upload videos and you'll see it here.</p>
+          }
         </ul>
       </div>
     </div>
